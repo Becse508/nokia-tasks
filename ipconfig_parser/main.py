d4323b8dd5b33a6803efe416751a6fc74336c47c
Kis Becse Gáspár <diak_106.kis.becse.gaspar@10.c.2025.com>
2026-04-16 15:01:44 +0200

    ipconfig parser

--- ipconfig_parser/main.py
+++ ipconfig_parser/main.py
@@ -1,9 +1,51 @@
 from pathlib import Path
+from json import dumps
 
+def parse_line(line: str) -> tuple[str, str]:
+    parts = line.split(":", 1)
+    if (len(parts) != 2):
+        return None, None
+    
+    # formazas
+    key = parts[0].replace(".", "").strip().lower().replace(" ", "_")
+    val = parts[1].strip()
+    if val == "" or val.isspace():
+        val = None
+    
+    return (key, val)
+
+def parse(text: str) -> dict[str, str]:
+    result = []
+    lines = text.split('\n')
+    
+    parsing = False
+    for line in lines:
+        if not parsing and "adapter" in line:
+            parsing = True
+            result.append({})
+            
+        elif line == "" or line.isspace():
+            # ha most kezdodott a blokk akkor hagyja, ha nem akkor vege
+            if parsing and len(result[-1]) != 0: 
+                parsing = False
+        
+        elif parsing:
+            key, val = parse_line(line)
+            if key != None: # None ertek megengedheto, kulcs nem
+                result[-1][key] = val
+            
+    return result
+        
 
 def main():
+    parsed_data = []
     for path in sorted(Path(".").glob("*.log")):
-        print(path.name)
+        parsed_data.append({
+            "file_name": path.name,
+            "adapters": parse(path.read_text())
+        })
+    
+    print(dumps(parsed_data, indent=2))
 
 
 if __name__ == "__main__":
--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,9 +1,18 @@
 from pathlib import Path
 
+def next_magic_num(n: int):
+    return n
+    
+
 
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    nums = [
+        next_magic_num(int(line))
+        for line in data.split('\n')
+        if line.isdecimal()
+    ]
+    print(nums)
 
 
 if __name__ == "__main__":
