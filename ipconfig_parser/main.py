f8b3ca4cc3f12973ba26b32f1041b68b0536ca02
Becse <kisbecse@gmail.com>
2026-04-16 16:50:39 +0200

    parking_calculator

--- ipconfig_parser/main.py
+++ ipconfig_parser/main.py
@@ -12,7 +12,7 @@ def parse_line(line: str) -> tuple[str, Any]:
     # formazas
     key = parts[0].replace(".", "").strip().lower().replace(" ", "_")
     val = parts[1].strip()
-    if key in KEYS_WITH_ARRAY_TYPE:
+    if (key in KEYS_WITH_ARRAY_TYPE):
         val = [x.strip() for x in val.split(',')]
     
     return (key, val)
@@ -24,17 +24,17 @@ def parse(text: str) -> dict[str, Any]:
     parsing = False
     just_started = False
     for line in lines:
-        if "adapter" in line and line.endswith(':') and not line.startswith(" "):
+        if ("adapter" in line and line.endswith(':') and not line.startswith(" ")):
             parsing = True
             just_started = True
             result.append({"adapter_name": line[:-2].strip()})
         
-        elif parsing and line.startswith(" "):
+        elif (parsing and line.startswith(" ")):
             key, val = parse_line(line)
-            if key != None:
+            if (key != None):
                 result[-1][key] = val
         
-        elif line == "" or line.isspace() or not line.startswith(" "):
+        elif (line == "" or line.isspace() or not line.startswith(" ")):
             if just_started:
                 just_started = False
             elif parsing:
--- parking_calculator/main.py
+++ parking_calculator/main.py
@@ -1,9 +1,56 @@
 from pathlib import Path
+from datetime import datetime, timedelta
+from sys import argv
 
+HOURS = 3600
+MINUTES = 60
+
+def parse(text: str) -> tuple[datetime, datetime] | None:
+    parts = text.split('		', 2)
+    if (len(parts) < 3):
+        return None
+    
+    return datetime.fromisoformat(parts[1]), datetime.fromisoformat(parts[2])
+
+
+def calc_time(start: datetime, end: datetime) -> timedelta:
+    if (start > end):
+        return timedelta(0)
+    return end - start
+    
+def calc_fine(time: timedelta, accurate: bool = False) -> float | int:
+    hours = 0
+    if (time.seconds != 0):
+        if (accurate): # masodpercre pontos szamolas
+            hours = time.seconds / HOURS
+        else: # minden megkezdett ora
+            hours = time.seconds // HOURS + 1
+    
+    # elso nap
+    if (time.days == 0):
+        # ingyenes 30 perc
+        if (time.seconds <= 30 * MINUTES):
+            return 0
+        # olcso 3 ora
+        if (hours <= 3):
+            return hours * 300
+
+        return 3 * 300 + (hours-3)*500
+        
+    
+    return time.days * 10000 + hours * 500
+    
 
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    accurate_fine = (len(argv) > 1 and argv[1] == "accurate")
+    
+    for line in data.split('\n')[2:]:
+        time = calc_time(*parse(line))
+        fine = calc_fine(time, accurate_fine)
+        
+        print(f"{time} -> {int(fine)}Ft")
+    
 
 
 if __name__ == "__main__":
