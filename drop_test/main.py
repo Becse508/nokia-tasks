dec6991e0f7912c17cd159575c663b5c2358b7f6
Becse <kisbecse@gmail.com>
2026-04-17 21:07:10 +0200

    drop_test

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,9 +1,24 @@
 from pathlib import Path
+from math import comb
+
+# milyen magassag lehetseges 'steps' lepessel
+def calc_possible_height(pieces, steps): 
+    return sum(comb(steps, n) for n in range(1, pieces+1))
+
+def min_num_drops(pieces, height):
+    for steps in range(1, height+1):
+        possible_height = calc_possible_height(pieces, steps)
+        if (possible_height >= height):
+            return steps
 
 
 def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    raw = Path("input.txt").read_text(encoding="utf-8")
+    data = [line.split(", ") for line in raw.splitlines()]
+    print("\n".join(
+        f"min_num_drops({pieces}, {height}) => {min_num_drops(int(pieces), int(height))}"
+        for pieces, height in data
+    ))
 
 
 if __name__ == "__main__":
