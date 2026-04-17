15b60848d6dea5bb7837507e186eff259c75a08c
Becse <kisbecse@gmail.com>
2026-04-17 17:00:05 +0200

    ipconfig_parser parameterek

--- ipconfig_parser/main.py
+++ ipconfig_parser/main.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 from json import dumps
 from typing import Any
+from sys import argv
+
+ARG_UNIFORM = "uniform" in argv
+ARG_FORCE = ("force" in argv) and ARG_UNIFORM
 
 KEYS_WITH_ARRAY_TYPE = ["dns_servers"]
-MUST_HAVE_KEYS = [
+UNIFORM_KEYS = [
     "adapter_name",
     "description",
     "physical_address",
@@ -28,18 +32,30 @@ def parse_line(line: str) -> tuple[str, Any]:
     
     return (key, val)
 
-def handle_must_have_keys(data: list[dict]):
+
+def make_uniform(data: list[dict], force: bool = False):
     for adapter in data:
-        for key in MUST_HAVE_KEYS:
+        for key in UNIFORM_KEYS:
             if not key in adapter:
                 adapter[key] = [] if key in KEYS_WITH_ARRAY_TYPE else ""
+    
+    if force:
+        data[:] = [
+            {
+                k: v for k, v in adapter.items()
+                if k in UNIFORM_KEYS
+            }
+            for adapter in data
+        ]
+    
+    
 
 # megnezem, hogy benne van-e az "adapter" szo a biztonsag kedveert
 is_adapter_name = lambda line: "adapter" in line and line.endswith(':') and not line.startswith(" ")
 is_end_of_block = lambda line: line == "" or line.isspace() or not line.startswith(" ")
 
 # biztonsagos parse
-def parse_safe(text: str) -> dict[str, Any]:
+def parse_safe(text: str, uniform: bool = False, force: bool = False) -> dict[str, Any]:
     result = []
     
     parsing = False
@@ -49,7 +65,7 @@ def parse_safe(text: str) -> dict[str, Any]:
         if is_adapter_name(line):
             parsing = True
             just_started = True
-            result.append({"adapter_name": line[:-2].strip()})
+            result.append({"adapter_name": line.strip()[:-1]})
         
         # blokk kozben
         elif (parsing and line.startswith(" ")):
@@ -64,12 +80,13 @@ def parse_safe(text: str) -> dict[str, Any]:
             elif parsing:
                 parsing = False
     
-    handle_must_have_keys(result)
+    if uniform:
+        make_uniform(result, force)
     return result
 
 
 # kevesbe biztonsagos de szebb a kod (tokeletesen mukodik sima ipconfiggal)
-def parse_short(text: str) -> dict[str, Any]:
+def parse_short(text: str, uniform: bool = False, force: bool = False) -> dict[str, Any]:
     result = []
     
     for line in text.split('\n'):
@@ -82,8 +99,8 @@ def parse_short(text: str) -> dict[str, Any]:
             key, val = parse_line(line)
             if (key != None):
                 result[-1][key] = val
-    
-    handle_must_have_keys(result)
+    if uniform:
+        make_uniform(result, force)
     return result
 
 def main():
@@ -91,7 +108,7 @@ def main():
     for path in Path(".").glob("*.log"):
         parsed_data.append({
             "file_name": path.name,
-            "adapters": parse_short(path.read_text(encoding='utf-8'))
+            "adapters": parse_safe(path.read_text(encoding='utf-8'), ARG_UNIFORM, ARG_FORCE)
         })
     
     print(dumps(parsed_data, indent=2))
